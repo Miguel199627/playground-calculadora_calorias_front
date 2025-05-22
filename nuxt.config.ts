8df164035c3df8430842f72b5a2723be248c5a01
Miguel Angel C <mcerquera@programarte.com.co>
2025-05-22 16:01:57 -0500

    20250522 - Subida de ajustes en el nuxt.config.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
+  ssr: false,
+  app: {
+    baseURL: "/pg-calculadora-calorias/", // solo si NO está en la raíz del dominio
+  },
   modules: ["@primevue/nuxt-module"],
   css: ["~/assets/css/main.css"],
   srcDir: "src/",
@@ -16,7 +20,8 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: "http://localhost:8080/playground-calculadora_calorias_api/api/",
+      apiBase: "http://localhost:8080/playground-calculadora_calorias_api/api/", // Local / Dev
+      // apiBase: "https://playground.marcsoft.com.co/pg-calculadora-calorias/playground-calculadora_calorias_api/api/", // Prod
     },
   },
   plugins: ["~/plugins/yup-locales.ts"],
